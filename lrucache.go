1be798fb9c67fa6e70704cf91913a5094bd4d134
Bill Dortch <bill.dortch@gmail.com>
2021-02-16 23:36:57 -0800

    Progress checkin.

--- lrucache.go
+++ lrucache.go
@@ -1,46 +1,68 @@
 package lrucache
 
 import (
+	"fmt"
 	"sync"
+	"time"
 )
 
 type LRUCache interface {
 	Get(key interface{}) (value interface{})
 	Put(key, value interface{})
 	Remove(key interface{}) (value interface{})
-	Contains(key interface{}) bool
 	Size() int
 	Capacity() int
-	TTLMillis() int
+	TTLSeconds() int64
 	Clear()
 }
 
 func New(capacity int) LRUCache {
-	// return &lrucache{
-	// 	capacity: capacity,
-	// }
-	panic("unimplemented")
+	if capacity <= 0 {
+		panic(fmt.Sprintf("invalid capacity: %d\n", capacity))
+	}
+	return &lrucache{
+		capacity: capacity,
+		hash:     make(map[interface{}]*entry),
+	}
 }
 
-func NewWithTTL(capacity, ttlMillis int) LRUCache {
-	panic("unimplemented")
+func NewWithTTL(capacity int, ttlSeconds int64) LRUCache {
+	if capacity <= 0 {
+		panic(fmt.Sprintf("invalid capacity: %d\n", capacity))
+	}
+	if ttlSeconds < 0 {
+		panic(fmt.Sprintf("invalid ttlMillis: %d\n", ttlSeconds))
+	}
+	c := &lrucache{
+		capacity:   capacity,
+		ttlSeconds: ttlSeconds,
+		hash:       make(map[interface{}]*entry),
+	}
+	go pruneExpiredEntries(c)
+	return c
+}
+
+func pruneExpiredEntries(c *lrucache) {
+	for {
+		time.Sleep(100 * time.Millisecond)
+	}
 }
 
 type entry struct {
-	next          *entry
-	prev          *entry
-	key           interface{}
-	value         interface{}
-	timeoutMillis int
+	next       *entry
+	prev       *entry
+	key        interface{}
+	value      interface{}
+	expireTime int64 // unix time seconds
 }
 
 type lrucache struct {
 	sync.Mutex
-	capacity  int
-	ttlMillis int
-	head      *entry
-	tail      *entry
-	hash      map[interface{}]*entry
+	capacity   int
+	ttlSeconds int64
+	head       *entry
+	tail       *entry
+	hash       map[interface{}]*entry
 }
 
 func (c *lrucache) Get(key interface{}) (value interface{}) {
@@ -83,6 +105,18 @@ func (c *lrucache) Put(key, value interface{}) {
 	}
 }
 
+func (c *lrucache) Remove(key interface{}) (value interface{}) {
+	c.Lock()
+	defer c.Unlock()
+	e := c.hash[key]
+	if e == nil {
+		return nil
+	}
+	c.unlink(e)
+	delete(c.hash, key)
+	return e.value
+}
+
 func (c *lrucache) Size() int {
 	c.Lock()
 	defer c.Unlock()
@@ -93,8 +127,8 @@ func (c *lrucache) Capacity() int {
 	return c.capacity
 }
 
-func (c *lrucache) TTLMillis() int {
-	return c.ttlMillis
+func (c *lrucache) TTLSeconds() int64 {
+	return c.ttlSeconds
 }
 
 func (c *lrucache) Clear() {
